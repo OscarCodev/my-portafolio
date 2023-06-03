0adcf63f3f5824ac6844a78c1b2e23d06ff4bf25
OscarCodev <OscarCodev@users.noreply.github.com>
2023-06-03 10:05:43 -0500

    titulo del header condicional

--- src/components/uis/Header.jsx
+++ src/components/uis/Header.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import styled from "styled-components";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function Header() {
+  const { pathname } = useLocation();
+
+  let miText;
+
+  if (pathname === "/") {
+    miText = (
+      <p className="primerPath">
+        oscar<span>Dev</span>
+      </p>
+    );
+  } else if (pathname === "/tecnologias") {
+    miText = <p className="segundoPath"> Tecnologías</p>;
+  } else if ("/experiencia") {
+    miText = <p className="tercerPath"> Experiencia</p>;
+  }
+
   return (
     <HeaderSup>
       <ContainerHeader>
-        <Text>
-          oscar<span>Dev</span>
-        </Text>
+        <Text>{miText}</Text>
       </ContainerHeader>
     </HeaderSup>
   );
@@ -37,11 +53,24 @@ const ContainerHeader = styled.div`
 const Text = styled.h1`
   font-family: "JetBrains Mono";
   font-size: 1.5rem;
-  color: #231e35;
-  & span {
+  p {
     background-clip: text;
     -webkit-background-clip: text;
     color: transparent;
-    background-image: linear-gradient(to right, #a4a1ff, #65baff);
+  }
+  p.primerPath {
+    color: #231e35;
+    span {
+      background-clip: text;
+      -webkit-background-clip: text;
+      color: transparent;
+      background-image: linear-gradient(to right, #a4a1ff, #65baff);
+    }
+  }
+  p.segundoPath {
+    background-image: linear-gradient(to right, #231e35, #e2a1d8);
+  }
+  p.tercerPath {
+    background-image: linear-gradient(to right, #231e35, #ffd972);
   }
 `;
