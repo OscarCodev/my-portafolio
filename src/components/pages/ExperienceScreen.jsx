68a642d75c03b7d357f8a8a6aa2b1db74e83e6dd
OscarCodev <OscarCodev@users.noreply.github.com>
2023-06-24 08:49:42 -0500

    maquetando el layout y añadiendo responsive

--- src/components/pages/ExperienceScreen.jsx
+++ src/components/pages/ExperienceScreen.jsx
@@ -6,27 +6,16 @@ function ExperienceScreen() {
   const data = ["NotesPro", "meditateMe", "pokeRank", "E-comerce"];
   return (
     <ContainerExperience transition-style="in:circle:top-right">
-      {data.map((card, i) => (
-        <ContainerProyect className="animate__animated animate__zoomIn">
-          <h2>Notes Pro</h2>
-          <div className="containerInfo">
-            <div className="containerImg">
-              <img src={proyect1} alt="img" />
-            </div>
-            <div className="containerTecnologias">
-              <div className="item"></div>
-              <div className="item"></div>
-              <div className="item"></div>
-              <div className="item"></div>
-              <div className="item"></div>
-            </div>
-            <div className="containerButtons">
-              <div className="buttonRepo"></div>
-              <div className="buttonLive">Live</div>
-            </div>
-          </div>
-        </ContainerProyect>
-      ))}
+      <div className="containerBoton"></div>
+      <div className="containerInfo">
+        <div className="containerNameImage">
+
+        </div>
+        <div className="containerDescription">
+
+        </div>
+      </div>
+      <div className="containerBoton"></div>
     </ContainerExperience>
   );
 }
@@ -34,98 +23,52 @@ function ExperienceScreen() {
 export default ExperienceScreen;
 
 const ContainerExperience = styled.div`
-  display: grid;
-  grid-template-columns: 1fr;
-  padding: 3rem;
-  gap: 3rem;
   width: 100%;
-  height: auto;
+  display: flex;
+  flex-direction: row;
+  height: calc(100vh - 120px);
   background-color: #231e35;
+  border: 1px solid white;
+  .containerBoton{
+    height: calc(100vh - 120px);
+    width: 50px;
+    background-color: coral;
+  }
+  .containerInfo{
+    height: calc(100vh - 120px);
+    flex-grow: 1;
+    background-color: cornflowerblue;
+
+    display: flex;
+    flex-direction: column;
+    .containerNameImage{
+      width: 100%;
+      height: 50%;
+      background-color: blue;
+    }
+    .containerDescription{
+      width: 100%;
+      height: 50%;
+      background-color: yellow;
+    }
+  }
   @media (min-width: 992px) {
-    grid-template-columns: 1fr 1fr 1fr;
+    height: calc(100vh - 60px); 
+    .containerBoton, .containerInfo{
+      height: calc(100vh - 60px)
+    }
+
+    .containerInfo{
+      flex-direction: row;
+      .containerNameImage{
+        width: 50%;
+        height: 100%;
+      }
+      .containerDescription{
+        width: 50%;
+        height: 100%;
+      }
+    }
   }
 `;
 
-const ContainerProyect = styled.div`
-  width: 100%;
-  height: 300px;
-  //border: 1px solid red;
-  font-family: "JetBrains Mono";
-  h2 {
-    color: #f9f9f9;
-    font-size: 1.2rem;
-    width: 100%;
-    height: 50px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  .containerInfo {
-    //border: 1px solid white;
-    border-radius: 1rem;
-    transition: all 0.3s;
-    &:hover {
-      transform: scale(0.99);
-    }
-  }
-  .containerImg {
-    width: 100%;
-    height: 170px;
-    border-top-right-radius: 1rem;
-    border-top-left-radius: 1rem;
-    border: 1px solid #535252;
-    border-left: 2px solid #535252;
-    img {
-      border-top-right-radius: 1rem;
-      border-top-left-radius: 1rem;
-      width: 100%;
-      height: 100%;
-    }
-  }
-  .containerTecnologias {
-    width: 100%;
-    height: 50px;
-    background-color: #353535;
-    border: 1px solid #535252;
-    border-left: 2px solid #535252;
-    display: flex;
-    justify-content: center;
-    gap: 0.5rem;
-    align-items: center;
-    .item {
-      width: 35px;
-      height: calc(40px - 0.5rem);
-      background-color: rgba(255, 255, 255, 0.1);
-      border-radius: 0.5rem;
-    }
-  }
-  .containerButtons {
-    width: 100%;
-    height: 50px;
-    background-color: #1e1e1e;
-    border-left: 2px solid #535252;
-    border-right: 1px solid #535252;
-    border-bottom: 1px solid #535252;
-    border-bottom-left-radius: 1rem;
-    border-bottom-right-radius: 1rem;
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-    .buttonRepo {
-      width: 35px;
-      height: calc(40px - 0.5rem);
-      background-color: rgba(255, 255, 255, 0.5);
-      border-radius: 0.5rem;
-    }
-    .buttonLive {
-      width: 100px;
-      height: calc(40px - 0.5rem);
-      background-color: coral;
-      border-radius: 1rem;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      color: #fff;
-    }
-  }
-`;
